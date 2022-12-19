9bf59abdf446fadfd167635a28892db5e0906a14
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-12-19 11:37:51 +0200

    handle errors are added to functions

--- contacts.js
+++ contacts.js
@@ -4,35 +4,54 @@ const nodeid = require("node-id");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-const listContacts = async () =>
-  JSON.parse(await fs.readFile(contactsPath, "utf8"));
+const listContacts = async () => {
+  try {
+    JSON.parse(await fs.readFile(contactsPath, "utf8"));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const getContactById = async (contactId) => {
-  if (!contactId) return console.error("Please, enter the contact id!");
-  const contacts = await listContacts();
-  return contacts.find((contact) => contact.id === contactId);
+  try {
+    if (!contactId) throw "Please, enter the contact id!";
+
+    const contacts = await listContacts();
+    return contacts.find((contact) => contact.id === contactId);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const removeContact = async (contactId) => {
-  if (!contactId) return console.error("Please, enter the contact id!");
-  const contacts = await listContacts();
-  const newContactsList = JSON.stringify(
-    contacts.filter((contact) => contact.id !== contactId)
-  );
-  await fs.writeFile(contactsPath, newContactsList, "utf8");
-  return contacts;
+  try {
+    if (!contactId) throw "Please, enter the contact id!";
+
+    const contacts = await listContacts();
+    const newContactsList = JSON.stringify(
+      contacts.filter((contact) => contact.id !== contactId)
+    );
+    await fs.writeFile(contactsPath, newContactsList, "utf8");
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const addContact = async (name, email, phone) => {
-  if (!name || !email || !phone)
-    return console.error(
-      "Please, enter the contact information (name/email/phone)!"
-    );
-  const contacts = await listContacts();
-  contacts.push({ id: nodeid(), name, email, phone });
-  const newContactsList = JSON.stringify(contacts);
-  await fs.writeFile(contactsPath, newContactsList, "utf8");
-  return contacts;
+  try {
+    if (!name || !email || !phone) {
+      throw "Please, enter the contact information (name/email/phone)!";
+    }
+
+    const contacts = await listContacts();
+    contacts.push({ id: nodeid(), name, email, phone });
+    const newContactsList = JSON.stringify(contacts);
+    await fs.writeFile(contactsPath, newContactsList, "utf8");
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
